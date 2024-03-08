973038282cb07c79218209ae40451446adf35631
Anup Jaiswal <150141325+anupjais@users.noreply.github.com>
2024-03-08 20:28:16 +0530

    Little bit updation into the visuals only

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 // alert("Special Buttons are not operational right not so please do the basic calculation")
+function backspace()
+{
+	var exp = document.form.div.op.value;
+	document.form.div.op.value = exp.substring(0, exp.length - 1); /* remove the element from total length ? 1 */
+}
+
+
+
+
+// function insert(num) 
+// {
+// 	document.form1.textview.value = document.form1.textview.value + num;
+// }
+
+// // Use equal() function to return the result based on passed values.
+// function equal()
+// {
+// 	var exp = document.form1.textview.value;
+// 	if(exp)
+// 	{
+// 		document.form1.textview.value = eval(exp)
+// 	}
+// }
+
+/* Here, we create a backspace() function to remove the number at the end of the numeric series in textview. */
+// function backspace()
+// {
+// 	var exp = document.form1.textview.value;
+// 	document.form1.textview.value = exp.substring(0, exp.length - 1); /* remove the element from total length ? 1 */
+// }
